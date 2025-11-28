4fb73be7f7e53a7c876a2718ae385ea6c9e4cf28
Javier Hernandez Vantuyl <98913872+JavierHernandezVantuyl@users.noreply.github.com>
2025-11-28 10:10:05 -0600

    Add Kubernetes cost optimization operator

---
+++ services/operator/handlers/__init__.py
@@ -0,0 +1,5 @@
+from handlers.optimization_handler import OptimizationHandler
+from handlers.workload_handler import WorkloadHandler
+from handlers.rollback_handler import RollbackHandler
+
+__all__ = ["OptimizationHandler", "WorkloadHandler", "RollbackHandler"]
